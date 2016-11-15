b4c1f9a89d4ed0a0b9ef08785d4b76176e3f4ad8
rgr-myrg <rpmyrg@gmail.com>
2016-11-15 10:53:47 -0500

    Added logger for debugging.

---
+++ src/main/java/edu/gatech/cms/logger/Log.java
@@ -0,0 +1,13 @@
+package edu.gatech.cms.logger;
+
+public final class Log {
+	public static boolean debug = false;
+
+	public static final boolean isDebug() {
+		return debug;
+	}
+
+	public static final void setDebug(boolean debug) {
+		Log.debug = debug;
+	}
+}
