8120885a2de72f08b717a9f81dead80b175d2307
rgr-myrg <rpmyrg@gmail.com>
2016-11-22 10:21:39 -0500

    Added buttons, progress bar, and on click stub methods.

--- src/main/java/edu/gatech/cms/controller/WelcomeController.java
+++ src/main/java/edu/gatech/cms/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package edu.gatech.cms.controller;
 
+import edu.gatech.cms.view.ApplicationView;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
@@ -8,9 +10,21 @@ public class WelcomeController {
 	public static final String TAG = WelcomeController.class.getSimpleName();
 	@FXML private Text textMessageBox;
 
-	@FXML protected void handleClickButtonAction(ActionEvent event) {
-		if (textMessageBox != null) {
-			textMessageBox.setText("Controller says: clicked!");
-		}
+//	@FXML protected void handleClickButtonAction(ActionEvent event) {
+//		if (textMessageBox != null) {
+//			textMessageBox.setText("Controller says: clicked!");
+//		}
+//	}
+
+	@FXML protected void onStartButtonClick(ActionEvent event) {
+		ApplicationView.getInstance().getStage().setTitle("onStartButtonClick");
+	}
+
+	@FXML protected void onResumeButtonClick(ActionEvent event) {
+		
+	}
+
+	@FXML protected void onExitButtonClick(ActionEvent event) {
+		Platform.exit();
 	}
 }
