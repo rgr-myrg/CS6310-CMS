2a1f2effbee802f45fba1602fdd84e3e0c046ab3
rgr-myrg <rpmyrg@gmail.com>
2016-11-23 14:36:02 -0500

    Added ui messages

---
+++ src/main/java/edu/gatech/cms/view/UiMessages.java
@@ -0,0 +1,9 @@
+package edu.gatech.cms.view;
+
+public final class UiMessages {
+	public static final String START_SEMESTER = "Please click 'Next' to begin Roster selections for the upcoming Semester.";
+	public static final String RESUME_SEMESTER = "You last completed Roster selections for Semester %d. " 
+			+ "Please click 'Resume' to continue Roster selections for the upcoming Semester or 'Start' to start over.";
+
+	public static final String APRIORI_HEADING = "Apriori Analysis for Semester %d";
+}
