21bf16c30e52c40c4c8f547b592e5de82d71acb3
rgr-myrg <rpmyrg@gmail.com>
2016-11-29 09:53:56 -0500

    Added window title constants

--- src/main/java/edu/gatech/cms/view/UiMessages.java
+++ src/main/java/edu/gatech/cms/view/UiMessages.java
@@ -10,6 +10,14 @@ public final class UiMessages {
 		+ "    Sebastian Popa" + System.lineSeparator()
 		+ "    Roger Mayorga";
 
+	public static final String WELCOME_WINDOW_TITLE = "CMS :: Welcome";
+
+	public static final String LOADING_WINDOW_TITLE = "CMS :: Loading";
+
+	public static final String APRIORI_WINDOW_TITLE = "CMS :: Apriori Analysis";
+
+	public static final String ABOUT_WINDOW_TITLE = "CMS :: About";
+
 	public static final String START_SEMESTER = "Please click 'Next' to begin Roster selections for the upcoming Semester.";
 
 	public static final String RESUME_SEMESTER = "You last completed Roster selections for Semester %d. " 
