ca4c7c72ea642f387ef4c1b7c7c9e97ac3b41ad7
rgr-myrg <rpmyrg@gmail.com>
2016-12-04 19:20:02 -0500

    total stats persistence implementation

--- src/main/java/edu/gatech/cms/data/TotalStatisticsData.java
+++ src/main/java/edu/gatech/cms/data/TotalStatisticsData.java
@@ -1,80 +1,59 @@
 package edu.gatech.cms.data;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.TreeMap;
 
+import edu.gatech.cms.logger.Log;
+import edu.gatech.cms.logger.Logger;
 import edu.gatech.cms.sql.TotalStatisticsTable;
 import edu.gatech.cms.util.DbHelper;
 
 public class TotalStatisticsData {
-
-	public static final void load() {
-		PreparedStatement preparedStatement = null;
-
-		try {
-			preparedStatement = DbHelper.getConnection().prepareStatement(TotalStatisticsTable.INSERT_EXAMINED);
-			preparedStatement.execute();
-
-			preparedStatement = DbHelper.getConnection().prepareStatement(TotalStatisticsTable.INSERT_GRANTED);
-			preparedStatement.execute();
-
-			preparedStatement = DbHelper.getConnection().prepareStatement(TotalStatisticsTable.INSERT_FAILED);
-			preparedStatement.execute();
-
-			preparedStatement = DbHelper.getConnection().prepareStatement(TotalStatisticsTable.INSERT_WAITLISTED);
-			preparedStatement.execute();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static final Map<String, Integer> loadFromDB() {
+	public static final Map<String, Integer> getTotalStatistics() {
 		final Map<String, Integer> totalStatistics = new TreeMap<>();
 		ResultSet resultSet = DbHelper.doSql(TotalStatisticsTable.SELECT_ALL);
 
-		if (resultSet != null) {
-			try {
-				while (resultSet.next()) {
-					String statName = resultSet.getString(TotalStatisticsTable.STATISTIC_NAME_COLUMN);
-					int statTotal = resultSet.getInt(TotalStatisticsTable.STATISTIC_TOTAL_COLUMN);
-					totalStatistics.put(statName, statTotal);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+		try {
+			while (resultSet.next()) {
+				String statName = resultSet.getString(TotalStatisticsTable.STATISTIC_NAME_COLUMN);
+				int statTotal = resultSet.getInt(TotalStatisticsTable.STATISTIC_TOTAL_COLUMN);
+				Logger.debug("STATS", statName + ":" + statTotal);
+				totalStatistics.put(statName, statTotal);
 			}
+
+			resultSet.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// We've arrived here for the first time during an 'initial' cycle since no db records exist.
+		if (totalStatistics.isEmpty()) {
+			totalStatistics.put(TotalStatisticsTable.EXAMINED, 0);
+			totalStatistics.put(TotalStatisticsTable.FAILED, 0);
+			totalStatistics.put(TotalStatisticsTable.GRANTED, 0);
+			totalStatistics.put(TotalStatisticsTable.WAITLISTED, 0);
 		}
 
 		return totalStatistics;
 	}
 
-	public static final void updateWithNewTotal(final String column, final int total) {
-		PreparedStatement preparedStatement = null;
-		try {
-			preparedStatement = DbHelper.getConnection().prepareStatement(TotalStatisticsTable.UDATE_STATISTIC_TOTAL);
-
-			preparedStatement.setInt(1, total);
-			preparedStatement.setString(2, column);
-		} catch (SQLException e) {
-			e.printStackTrace();
+	public static final void saveTotalStats(final Map<String, Integer> totalStatistics) {
+		for (Map.Entry<String,Integer> item : totalStatistics.entrySet()) {
+			updateTotalWithKeyValue(item.getKey(), item.getValue());
 		}
 	}
 
-	public static final void updateExaminedTotal(final int total) {
-		updateWithNewTotal(TotalStatisticsTable.EXAMINED, total);
-	}
+	public static final void updateTotalWithKeyValue(final String key, final int total) {
+		String updateSql = TotalStatisticsTable.UDATE_STATISTIC_TOTAL
+				.replaceFirst("\\?", String.valueOf(key))
+				.replaceFirst("\\?", String.valueOf(total));
 
-	public static final void updateGrantedTotal(final int total) {
-		updateWithNewTotal(TotalStatisticsTable.GRANTED, total);
-	}
+		boolean success = DbHelper.doUpdateSql(updateSql);
 
-	public static final void updateFailedTotal(final int total) {
-		updateWithNewTotal(TotalStatisticsTable.FAILED, total);
-	}
-
-	public static final void updateWaitlistedTotal(final int total) {
-		updateWithNewTotal(TotalStatisticsTable.WAITLISTED, total);
+		if (Log.isDebug()) {
+			Logger.debug("TotalStatisticsData", updateSql + " => " + success);
+		}
 	}
 }
